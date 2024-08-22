9b4cd11cde94a2f385d8abfd948facbe205a9366
Muhsin <thottungalmuhsin@gmail.com>
2024-08-23 00:53:09 +0530

    mongoAddedToenv

--- confiq/mongodb.js
+++ confiq/mongodb.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose')
-const mongoDb=mongoose.connect('mongodb://127.0.0.1:27017/HeadphoneZone').then(res=>{
+const mongoDb=mongoose.connect(process.env.MONGO_URL).then(res=>{
     console.log("mongodb connected");
 })
 .catch(err=>{
